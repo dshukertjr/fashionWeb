a899d18da312d26ebd65419fc16b2f51c27aa755
Tyler S <dshukertjr@gmail.com>
2017-11-05 02:11:29 +0900

    created function to add timestamp to the posts

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,24 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+exports.createPost = functions.firestore
+  .document('posts/{postId}')
+  .onCreate(event => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    var newValue = event.data.data()
+
+    // access a particular field as you would any JS property
+    var name = newValue.name
+
+    // perform desired operations ...
+    const createTime = admin.database.ServerValue.TIMESTAMP
+
+    console.log("timestamp", createTime)
+
+    return event.data.ref.set({
+      createTime: createTime
+    }, {merge: true});
+})
